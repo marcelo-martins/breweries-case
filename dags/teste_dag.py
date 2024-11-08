0471f7407e0f1cf8a343a95696e6090d534d46e4
Marcelo Martins <marcelobmartins219@hotmail.com>
2024-11-07 22:19:57 -0300

    Upload to bronze layer succeeded

--- dags/teste_dag.py
+++ dags/teste_dag.py
@@ -1,44 +1,61 @@
-from datetime import datetime, timedelta
 from airflow import DAG
-from airflow.operators.dummy import DummyOperator
-from airflow.operators.python import PythonOperator
+from airflow.providers.amazon.aws.hooks.s3 import S3Hook
+from airflow.operators.python_operator import PythonOperator
+import requests
+import pandas as pd
+import io
+from datetime import datetime, timedelta
 
 # Função de teste para o PythonOperator
-def hello_world():
-    print("Hello, Airflow!")
+def fetch_brewery_data_to_s3():
+    API_URL = 'https://api.openbrewerydb.org/breweries'
+    
+    # Fazendo a requisição para a API
+    response = requests.get(API_URL)
+    
+    if response.status_code == 200:
+        # Convertendo a resposta para JSON
+        data = response.json()
+        
+        # Convertendo para DataFrame
+        df = pd.DataFrame(data)
+        
+        # Salvando em um buffer (em memória)
+        csv_buffer = io.StringIO()
+        df.to_csv(csv_buffer, index=False)
+        csv_buffer.seek(0)
+        
+        # Conectando ao S3
+        s3_hook = S3Hook(aws_conn_id='awsconn')
+        
+        # Nome do bucket e caminho no S3
+        bucket_name = 'abi-bees-case-2'
+        s3_key = 'bronze/brewery_data.csv'
+        
+        # Enviando o CSV para o S3
+        s3_hook.load_string(csv_buffer.getvalue(), key=s3_key, bucket_name=bucket_name, replace=True)
 
-# Configuração básica do DAG
 default_args = {
     'owner': 'airflow',
     'retries': 1,
     'retry_delay': timedelta(minutes=5),
 }
 
-# Criando o DAG
-with DAG(
-    dag_id='dag_teste',
-    default_args=default_args,
-    description='DAG de teste simples',
-    schedule_interval=timedelta(days=1),
-    start_date=datetime(2023, 1, 1),
+# Definindo o DAG
+dag = DAG(
+    'fetch_brewery_data_to_s3',
+    description='Busca dados de breweries da API e salva no S3',
+    schedule_interval=None,  # Pode ser ajustado para rodar periodicamente
+    start_date=datetime(2024, 11, 7),
     catchup=False,
-) as dag:
+)
 
-    # Operador Dummy para uma tarefa de início
-    start = DummyOperator(
-        task_id='start'
-    )
+# Definindo o operador Python para rodar a função
+fetch_data_task = PythonOperator(
+    task_id='fetch_brewery_data',
+    python_callable=fetch_brewery_data_to_s3,
+    dag=dag,
+)
 
-    # Operador Python para executar a função hello_world
-    hello_task = PythonOperator(
-        task_id='hello_task',
-        python_callable=hello_world,
-    )
-
-    # Operador Dummy para uma tarefa de fim
-    end = DummyOperator(
-        task_id='end'
-    )
-
-    # Definindo a ordem das tarefas
-    start >> hello_task >> end
+# Adicionando as dependências, neste caso é uma tarefa única
+fetch_data_task
